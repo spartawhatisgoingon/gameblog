4bd83842883a7422f88747ac24dc1b1753cec01f
namsanghyeok <artexi1464.2@gmail.com>
2024-06-05 17:18:41 +0900

    [#17] 🎉 add: AOP를 이용한 로그 추가
    로그 depth 와 트랜잭션 id 를 로그에 넣었습니다.

--- src/main/java/sparta/gameblog/dto/PostCreateRequestDto.java
+++ src/main/java/sparta/gameblog/dto/PostCreateRequestDto.java
@@ -3,9 +3,11 @@ package sparta.gameblog.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class PostCreateRequestDto {
     @NotBlank
     private String title;
